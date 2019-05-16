e75ad6e7c4735c72b4eaf817f53151fe390f821f
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-05-16 22:08:06 +0900

    add valid and hole loss

--- src/loss.py
+++ src/loss.py
@@ -2,11 +2,6 @@ import torch
 import torch.nn as nn
 
 
-def tvloss(input):
-    return torch.mean(torch.abs(input[:, :, :, :-1] - input[:, :, :, 1:])) + \
-           torch.mean(torch.abs(input[:, :, :-1, :] - input[:, :, :, 1:]))
-
-
 class InpaintingLoss(nn.Module):
     def __init__(self, extractor):
         super(InpantingLoss, self).__init__()
@@ -15,6 +10,21 @@ class InpaintingLoss(nn.Module):
         self.extractor = extractor
 
     def forward(self, input, mask, output, gt):
-        pass
+        # Non-hole pixels directly set to ground truth
+        comp = mask * input + (1 - mask) * output
+
+        tv_loss = torch.sum(torch.abs(input[:, :, :, :-1] - input[:, :, :, 1:])) \
+                  + torch.sum(torch.abs(input[:, :, :-1, :] - input[:, :, 1:, :]))
+        hole_loss = l1((1 - mask) * (output - gt))
+        valid_loss = l1(mask * (output - gt))
+        perc_loss = 
+
+
+        total_loss = valid_loss + 6*hole_loss + 0.05*perceptual_loss \
+                     + 120*(style_out_loss+style_comp_loss) + 0.1*tv_loss
+
+        return total_loss
+
+
 
 
