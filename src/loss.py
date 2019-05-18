2afcbaac920e87ac8bfc22a31c55e937b05f521c
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-05-18 13:05:53 +0900

    change the location of normalization from data transformation to before inputting to VGG

--- src/loss.py
+++ src/loss.py
@@ -48,11 +48,15 @@ class InpaintingLoss(nn.Module):
 
 # The network of extracting the feature for perceptual and style loss
 class VGG16FeatureExtractor(nn.Module):
+    MEAN = [0.485, 0.456, 0.406]
+    STD = [0.229, 0.224, 0.225]
+
     def __init__(self):
         super().__init__()
         vgg16 = models.vgg16(pretrained=True)
+        normalization = Normalization(self.MEAN, self.STD)
         # Define the each feature exractor
-        self.enc_1 = nn.Sequential(*vgg16.features[:5])
+        self.enc_1 = nn.Sequential(normalization, *vgg16.features[:5])
         self.enc_2 = nn.Sequential(*vgg16.features[5:10])
         self.enc_3 = nn.Sequential(*vgg16.features[10:17])
 
@@ -67,6 +71,21 @@ class VGG16FeatureExtractor(nn.Module):
             feature_map = getattr(self, 'enc_{}'.format(i+1))(feature_maps[-1])
             feature_maps.append(feature_map)
         return feature_maps[1:]
+
+
+# Normalization Layer for VGG
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
 
 
 # Calcurate the Gram Matrix of feature maps
