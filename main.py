e3c62e2d30950703be2b1c40c1ff67160495ed3c
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-05-19 17:36:53 +0900

    fix bugs and add the loading model and optimizer for fine-tuning

--- main.py
+++ main.py
@@ -7,25 +7,16 @@ from torch.utils.data import DataLoader
 from src.config import get_config
 from src.dataset import Places2
 from src.model import PConvUNet
-from src.train import train
-from src.utils import Config, init_xavier
-from src import const
+from src.loss import InpaintingLoss, VGG16FeatureExtractor
+from src.train import Trainer
+from src.utils import Config, load_ckpt
 
-
-# the information of comet-ml
-API_KEY = 
-PROJECT_NAME = 
-WORKSPACE =
-
-# data root path
-IMG_ROOT = './data'
-MASK_ROOT = './data/mask'
 
 # set the config
 config = Config(get_config())
 
 # Define the used device
-device = torch.device('cuda:{}'.format(config.cuda_num)
+device = torch.device('cuda:{}'.format(config.cuda_id)
                       if torch.cuda.is_available() else 'cpu')
 
 # Define the model
@@ -36,50 +27,57 @@ img_tf = transforms.ToTensor()
 mask_tf = transforms.ToTensor()
 
 # Define the Validation set
-dataset_val = Places2(IMG_ROOT,
-                      MASK_ROOT,
+dataset_val = Places2(config.img_root,
+                      config.mask_root,
                       img_tf,
                       mask_tf,
-                      data='val').to(device)
+                      data='val')
 
 # Set the configuration for training
 if config.mode == 'train':
     # set the comet-ml
     if config.comet:
-        experiment = Experiment(api_key=API_KEY,
-                                project_name=PROJECT_NAME,
-                                workspace=WORKSPACE)
+        experiment = Experiment(api_key=config.api_key,
+                                project_name=config.project_name,
+                                workspace=config.workspace)
         experiment.log_parameters(config)
     else:
         experiment = None
 
     # Define the Places2 Dataset and Data Loader
-    dataset_train = Places2(IMG_ROOT,
-                            MASK_ROOT,
+    dataset_train = Places2(config.img_root,
+                            config.mask_root,
                             img_tf,
                             mask_tf,
-                            data='train').to(device)
-
-    # Define the model initializer
-    model.apply(init_xavier)
+                            data='train')
 
     # Define the Loss fucntion
-    criterion = InpaintingLoss()
+    criterion = InpaintingLoss(VGG16FeatureExtractor()).to(device)
     # Define the Optimizer
+    lr = config.finetune_lr if config.finetune else config.initial_lr
     if config.optim == 'Adam':
-        optimizer = torch.optim.Adam(model.parameters(),
-                                     lr=config.lr,
+        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
+                                     lr=lr,
                                      weight_decay=config.weight_decay)
     elif config.optim == 'SGD':
-        optimizer = torch.optim.SGD(model.parameters(),
-                                    lr=config.lr,
+        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()),
+                                    lr=lr,
                                     momentum=config.momentum,
                                     weight_decay=config.weight_decay)
 
-    trainer = Trainer(config, device, model, dataloader_train,
+    start_iter = 0
+    if config.resume:
+        start_iter = load_ckpt(config.resume,
+                               [('model', model)],
+                               [('optimizer', optimizer)])
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        print('Starting from iter ', start_iter)
+
+    trainer = Trainer(start_iter, config, device, model, dataset_train,
                       dataset_val, criterion, optimizer, experiment=experiment)
     if config.comet:
-        with experiment.train()
+        with experiment.train():
             trainer.iterate(config.num_iter)
     else:
         trainer.iterate(config.num_iter)
