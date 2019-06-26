1e20dc8fc1ec2c195e11d4c4bd3d40064a8f9106
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-06-26 23:54:47 +0900

    Fix some bugs

--- predict.py
+++ predict.py
@@ -1,22 +1,24 @@
+import argparse
 import os
 from PIL import Image
 import torch
 from torchvision import transforms
 
 from src.model import PConvUNet
-from src.utils import create_ckpt_dir
 
-# create the checkpoint directory
-ckpt = create_ckpt_dir()
+parser = argparse.ArgumentParser(description="Specify the inputs")
+parser.add_argument('--img', type=str, default="examples/img0.jpg")
+parser.add_argument('--mask', type=str, default="examples/mask0.png")
+parser.add_argument('--model', type=str, default="pretrained_pconv.pth")
+args = parser.parse_args()
 
 # Define the used device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Define the model
 print("Loading the Model...")
-MODEL_PATH = "pretrained_pconv.pth"
 model = PConvUNet(finetune=False, layer_size=7)
-model.load_state_dict(torch.load(MODEL_PATH)['model'])
+model.load_state_dict(torch.load(args.model, map_location=device)['model'])
 model.to(device)
 
 # Data Transformation
@@ -27,26 +29,27 @@ mask_tf = transforms.Compose([
             transforms.ToTensor()
             ])
 
-IMAGE_PATH = "./example/img01.png"
-MASK_PATH = "./example/mask01.png"
-img = Image.open(IMAGE_PATH)
+print("Loading the inputs...")
+img = Image.open(args.img)
 img = img_tf(img.convert('RGB'))
-mask = Image.open(MASK_PATH)
+mask = Image.open(args.mask)
 mask = mask_tf(mask.convert('RGB'))
 gt = img
 img = img * mask
 
 
 # model prediction
+print("Model Prediction...")
 model.eval()
 with torch.no_grad():
-    output, _ = model(img.to(device), mask.to(device))
-output = output.to(torch.device('cpu'))
+    output, _ = model(img.unsqueeze(0).to(device),
+                      mask.unsqueeze(0).to(device))
+output = output.to(torch.device('cpu')).squeeze()
 output_comp = mask * img + (1 - mask) * output
 
 # save the output image
-out = output_comp.numpy()
-to_pil = transforms.TOPILImage()
-out = to_pil(out)
-img_name = IMAGE_PATH.split('/')[-1]
-out.save(os.path.join(ckpt, "out_{}".format(img_name)))
+print("Saving the output...")
+to_pil = transforms.ToPILImage()
+out = to_pil(output_comp)
+img_name = args.img.split('/')[-1]
+out.save(os.path.join("examples", "out_{}".format(img_name)))
