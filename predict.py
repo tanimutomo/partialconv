0a8cdc60920d5270cab355883c37ebc7690888c2
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2020-04-29 23:55:32 +0900

    Fix output artifacts

--- predict.py
+++ predict.py
@@ -2,54 +2,53 @@ import argparse
 import os
 from PIL import Image
 import torch
-from torchvision import transforms
+import torchvision.transforms.functional as F
 
 from src.model import PConvUNet
 
-parser = argparse.ArgumentParser(description="Specify the inputs")
-parser.add_argument('--img', type=str, default="examples/img0.jpg")
-parser.add_argument('--mask', type=str, default="examples/mask0.png")
-parser.add_argument('--model', type=str, default="pretrained_pconv.pth")
-args = parser.parse_args()
 
-# Define the used device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def main(args):
+    # Define the used device
+    device = torch.device(f"cuda:{args.gpu_id}" if torch.cuda.is_available() else "cpu")
 
-# Define the model
-print("Loading the Model...")
-model = PConvUNet(finetune=False, layer_size=7)
-model.load_state_dict(torch.load(args.model, map_location=device)['model'])
-model.to(device)
+    # Define the model
+    print("Loading the Model...")
+    model = PConvUNet(finetune=False, layer_size=7)
+    model.load_state_dict(torch.load(args.model, map_location=device)['model'])
+    model.to(device)
 
-# Data Transformation
-img_tf = transforms.Compose([
-            transforms.ToTensor()
-            ])
-mask_tf = transforms.Compose([
-            transforms.ToTensor()
-            ])
+    # Loading Input and Mask
+    print("Loading the inputs...")
+    import numpy as np
+    org = Image.open(args.img)
+    org = F.to_tensor(org.convert('RGB'))
+    mask = Image.open(args.mask)
+    mask = F.to_tensor(mask.convert('RGB'))
+    inp = org * mask
 
-print("Loading the inputs...")
-img = Image.open(args.img)
-img = img_tf(img.convert('RGB'))
-mask = Image.open(args.mask)
-mask = mask_tf(mask.convert('RGB'))
-gt = img
-img = img * mask
+    # Model prediction
+    print("Model Prediction...")
+    model.eval()
+    with torch.no_grad():
+        raw_out, _ = model(inp.unsqueeze(0).to(device),
+                        mask.unsqueeze(0).to(device))
+    raw_out = raw_out.to(torch.device('cpu')).squeeze()
+    raw_out = raw_out.clamp(0.0, 1.0)
+    out = mask * inp + (1 - mask) * raw_out
+
+    # Saving an output image
+    print("Saving the output...")
+    out = F.to_pil_image(out)
+    img_name = args.img.split('/')[-1]
+    out.save(os.path.join("examples", "out_{}".format(img_name)))
 
 
-# model prediction
-print("Model Prediction...")
-model.eval()
-with torch.no_grad():
-    output, _ = model(img.unsqueeze(0).to(device),
-                      mask.unsqueeze(0).to(device))
-output = output.to(torch.device('cpu')).squeeze()
-output_comp = mask * img + (1 - mask) * output
-
-# save the output image
-print("Saving the output...")
-to_pil = transforms.ToPILImage()
-out = to_pil(output_comp)
-img_name = args.img.split('/')[-1]
-out.save(os.path.join("examples", "out_{}".format(img_name)))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Specify the inputs")
+    parser.add_argument('--img', type=str, default="examples/img0.jpg")
+    parser.add_argument('--mask', type=str, default="examples/mask0.png")
+    parser.add_argument('--model', type=str, default="pretrained_pconv.pth")
+    parser.add_argument('--gpu_id', type=int, default=0)
+    args = parser.parse_args()
+    
+    main(args)
